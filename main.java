4e6fedf23464694840724d709170fdc9b79f9bf0
trabbani12 <trabbani12@gmail.com>
2017-03-07 16:41:43 -0800

    Added main file and training/test data

---
+++ main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class main
+{
+  public static void main(String[] args)
+    {
+    Scanner readFile = new Scanner("hw5train.txt");
+    while (readFile.hasNext());
+      String temp = readFile.nextLine();
+
+  }
+}
